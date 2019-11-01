8e9de1d6478824f42604abbe8e7fedbfd8762d8b
Michael Karcher <github@mkarcher.dialup.fu-berlin.de>
2019-11-01 12:34:14 +0100

    single-step emulator: Add shift/rotate emulation
    The idea is to hot-patch a generic code fragment to emulate the current instruction and return into that fragment. The fragment ends with a far jump past the emulated instruction.

--- enter.asm
+++ enter.asm
@@ -127,7 +127,50 @@ above_enter:
 	je		emulate_leave
 	jmp		emu_exit
 between_6A_and_C8:
+	cmp		al, 0C0h
+	jb		between_6A_and_C0
+	cmp		al, 0C1h
+	ja		between_C1_and_C8
+
+	add		al, 12h		; C0 -> D2, C1 -> D3
+	mov		BYTE PTR [cs:sr_patch_opcode], al
+	lodsb				; Fetch mod/RM byte
+	mov		ah, al
+	or		al, 0C0h	; set memory operand to AX/AL (for shift/rotate)
+	and		al, 0F8h
+	mov		BYTE PTR [cs:sr_patch_opcode+1], al
+	and		ah, 0C7h	; set register operand to AX (for XCHG instructions)
+	mov		BYTE PTR [cs:sr_xchg2 + 1], ah
+	mov		BYTE PTR [cs:sr_xchg1 + 1], ah
+	cmp		ah, 06h		; special encoding for baseless 16-bit address
+	jne		not_mem16
+	mov		ah, 80h		; replace mode by something with a 16-bit offset
+not_mem16:
+	sub		ah, 40h		; 00..3F = 8bit, 40..7F = 16bit, 80..FF = nothing
+	js		no_offset
+	cmp		ah, 40h
+	jae		offset16
+	lodsb
+	mov     ah, 90h
+	jmp		SHORT ofs_common
+offset16:
+	lodsw
+	jmp		SHORT ofs_common
+no_offset:
+	mov		ax, 9090h
+ofs_common:
+	mov		WORD PTR [cs:sr_xchg2 + 2], ax
+	mov		WORD PTR [cs:sr_xchg1 + 2], ax
+	lodsb
+	mov		BYTE PTR [cs:sr_patch_count], al
+	mov		WORD PTR [cs:sr_dest_offset], si
+	mov     WORD PTR [cs:sr_dest_segment], ds
+	mov		[bp + 2], cs ; return CS
+	mov     [bp + 0], OFFSET sr_inject ; return IP
+
 below_push16:
+between_6A_and_C0:
+between_C1_and_C8:
 emu_exit:
 	mov		si, 1234h
 	emuss_oldds = $-2
@@ -139,4 +182,35 @@ emu_exit:
 	mov     ax, 1234h
 	emuss_oldax = $-2
 	iret
+
+; This code gets injected after IRET by patching the return address
+; to point here and patching the original return address into this
+; code. This design was chosen, because
+;  1. rcl/rcr takes the carry flag as input, so some kind of popf is
+;     needed anyway
+;  2. flag output is relevant, so we don't want to end with IRET
+;  3. we want all registers restored, so we can blindly copy the
+;     addressing scheme from the 286 instruction into the xchg
+;     instructions
+; This design is silly, because
+;  1. after IRET, the TF is set, so this code gets traced by the emulator
+; This design is broken, because
+;  1. after IRET, the IF is set, so an interrupt might occur while running
+;     the self-modifying, non-reentrant code here. This does not matter
+;     until we add a TF setting interrupt handler wrapper, though.
+sr_inject:
+	sr_xchg1 = $
+	xchg	ax, WORD PTR [ds:1234h]		; TODO: segment prefixes
+	push	cx
+	mov		cl, 12h
+	sr_patch_count = $-1
+	sr_patch_opcode = $
+	shl		ax, cl
+	pop		cx
+	sr_xchg2 = $
+	xchg	ax, WORD PTR [ds:1234h]		; TODO: segment prefixes
+	;jmp		FAR 1234h:5678h
+	db	0EAh
+sr_dest_offset dw 5678h
+sr_dest_segment dw 1234h
 END
