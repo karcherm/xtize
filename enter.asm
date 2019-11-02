c8fdde2d4b8e58f3e14ff20595c1431223793e84
Michael Karcher <github@mkarcher.dialup.fu-berlin.de>
2019-11-02 16:03:22 +0100

    general: convert tabs to spaces
    Hard tabs with width 4 are displayed erratically by all modern tools. Kick them.

--- enter.asm
+++ enter.asm
@@ -19,14 +19,14 @@ oldint16ptr dd ?
     pop     bp
     iret
 
-oldsp	dw ?
-oldss	dw ?
+oldsp   dw ?
+oldss   dw ?
 
 _EnterSingleStep:
-PUBLIC	_EnterSingleStep
-	push 	bp
-	mov  	bp,sp
-	push	ds
+PUBLIC  _EnterSingleStep
+    push    bp
+    mov     bp,sp
+    push    ds
     ; Install TF-resetting helper
     mov     ax, 3516h
     int     21h
@@ -38,103 +38,103 @@ PUBLIC	_EnterSingleStep
     mov     dx, OFFSET HelperInt16
     int     21h
     ; Activate callee PSP
-	les		si, [bp + 6]   	  ; state block
-	mov		bx, [es:si + 22]  ; new PSP
-	mov		ah, 50h
-	int		21h
+    les     si, [bp + 6]      ; state block
+    mov     bx, [es:si + 22]  ; new PSP
+    mov     ah, 50h
+    int     21h
 
     ; Set up callee entry point registers (including TF) and save old stack
-	mov		bx, [es:si + 22]
-	mov		di, [es:si + 14]  ; new stack pointer
-	mov		[cs:oldsp], sp
-	mov		[cs:oldss], ss
-	mov		ss, [es:si + 16]
-	mov 	sp, di
-	xor     ax, ax
-	push    ax  		; COM return address (PSP:0)
-	mov		ah, 3       ; Set IF and TF
-	push    ax
-	push	[WORD PTR es:si + 20]       ; new CS:IP
-	push	[WORD PTR es:si + 18]
-	mov 	ds, bx
-	mov 	es, bx
-	mov 	[ds:WORD PTR 0Ch], cs
-	mov     [ds:WORD PTR 0Ah], offset RETURNTO
-	iret
+    mov     bx, [es:si + 22]
+    mov     di, [es:si + 14]  ; new stack pointer
+    mov     [cs:oldsp], sp
+    mov     [cs:oldss], ss
+    mov     ss, [es:si + 16]
+    mov     sp, di
+    xor     ax, ax
+    push    ax          ; COM return address (PSP:0)
+    mov     ah, 3       ; Set IF and TF
+    push    ax
+    push    [WORD PTR es:si + 20]       ; new CS:IP
+    push    [WORD PTR es:si + 18]
+    mov     ds, bx
+    mov     es, bx
+    mov     [ds:WORD PTR 0Ch], cs
+    mov     [ds:WORD PTR 0Ah], offset RETURNTO
+    iret
 RETURNTO:
     ; Restore stack
-	mov		ss, [cs:oldss]
-	mov		sp, [cs:oldsp]
+    mov     ss, [cs:oldss]
+    mov     sp, [cs:oldsp]
     ; Uninstall helpers
     mov     ax, 2516h
     lds     dx, DWORD PTR [cs:oldint16ptr]
     int     21h
     ; Return to caller environment
-	pop		ds
-	pop		bp
-	retf
+    pop     ds
+    pop     bp
+    retf
 
-count	dd	0
+count   dd  0
 _CountingSS:
 PUBLIC _CountingSS
-	add     [cs:WORD PTR count], 1
-	adc     [cs:WORD PTR count+2], 0
-	iret
+    add     [cs:WORD PTR count], 1
+    adc     [cs:WORD PTR count+2], 0
+    iret
 
 _GetCount:
 PUBLIC _GetCount
-	mov		ax, [cs:WORD PTR count]
-	mov		dx, [cs:WORD PTR count+2]
-	retf
+    mov     ax, [cs:WORD PTR count]
+    mov     dx, [cs:WORD PTR count+2]
+    retf
 
 ; Error message with courtesy to a well-known un-fellow from the MBR
 uhmsg:
-	db 		"286 CHECK", 0
+    db      "286 CHECK", 0
 unhandled:
-	mov		ax,1h
-	int		10h
-	mov		ah, 0eh
-	mov		si, offset uhmsg
+    mov     ax,1h
+    int     10h
+    mov     ah, 0eh
+    mov     si, offset uhmsg
 uhloop:
-	lods    BYTE PTR [cs:si]
-	int		10h
-	cmp		al, 0
-	jne		uhloop
-	hlt
-	jmp 	$-1
+    lods    BYTE PTR [cs:si]
+    int     10h
+    cmp     al, 0
+    jne     uhloop
+    hlt
+    jmp     $-1
 
 emulate_enter:
-	cmp     byte ptr [ds:si+2], 0h  ; we don't support nested frame enter
-	jne		unhandled
-	mov     ax, WORD PTR [cs:emuss_oldbp]
-	xchg	ax, [bp + 4] ; replace caller flags by old bp (PUSH BP)
+    cmp     byte ptr [ds:si+2], 0h  ; we don't support nested frame enter
+    jne     unhandled
+    mov     ax, WORD PTR [cs:emuss_oldbp]
+    xchg    ax, [bp + 4] ; replace caller flags by old bp (PUSH BP)
 
-	add     sp, 4                 ; for PUSH BP
-	mov     WORD PTR [cs:emuss_oldbp], sp  ; MOV BP, SP part of ENTER
+    add     sp, 4                 ; for PUSH BP
+    mov     WORD PTR [cs:emuss_oldbp], sp  ; MOV BP, SP part of ENTER
 
-	sub		sp, word ptr [si]     ; SUB SP,#imm16 part of ENTER
-	add     si, 3
+    sub     sp, word ptr [si]     ; SUB SP,#imm16 part of ENTER
+    add     si, 3
 elcommon:
-	sub		sp, 6
+    sub     sp, 6
 elcommon_:
-	mov     bp, sp
-	mov     [bp + 0], si          ; original IP
-	mov     [bp + 2], ds          ; original CS
-	mov     [bp + 4], ax          ; original flags
-	jmp		emu_redo
+    mov     bp, sp
+    mov     [bp + 0], si          ; original IP
+    mov     [bp + 2], ds          ; original CS
+    mov     [bp + 4], ax          ; original flags
+    jmp     emu_redo
 
 emulate_leave:
-	mov		ax, [bp + 4] 		  ; get original flags
-	mov		sp, WORD PTR [cs:emuss_oldbp] ; MOV SP, BP part of LEAVE
-	pop		WORD PTR [cs:emuss_oldbp]	  ; POP BP part of LEAVE
-	jmp		elcommon
+    mov     ax, [bp + 4]          ; get original flags
+    mov     sp, WORD PTR [cs:emuss_oldbp] ; MOV SP, BP part of LEAVE
+    pop     WORD PTR [cs:emuss_oldbp]     ; POP BP part of LEAVE
+    jmp     elcommon
 
 emulate_push16:
-	lodsw
+    lodsw
 pushcommon:
-	xchg	ax, [bp + 4] ; replace caller flags by old bp (PUSH BP)
-	sub		sp, 2
-	jmp		elcommon_
+    xchg    ax, [bp + 4] ; replace caller flags by old bp (PUSH BP)
+    sub     sp, 2
+    jmp     elcommon_
 
 ; Entry-Points:
 ;  emu_redo - jump here instead of emu_exit if an instruction has
@@ -145,99 +145,99 @@ pushcommon:
 ;                interrupt stack frame returning to the final target
 _EmulatingSS:
 PUBLIC _EmulatingSS
-	mov		WORD PTR [cs:emuss_oldbp], bp
+    mov     WORD PTR [cs:emuss_oldbp], bp
 emu_reenter:
-	mov		WORD PTR [cs:emuss_oldsi], si
-	mov		WORD PTR [cs:emuss_oldds], ds
-	mov		WORD PTR [cs:emuss_oldax], ax
+    mov     WORD PTR [cs:emuss_oldsi], si
+    mov     WORD PTR [cs:emuss_oldds], ds
+    mov     WORD PTR [cs:emuss_oldax], ax
 emu_redo:
-	mov		bp, sp
-	mov		ds, [bp + 2] ; caller CS
-	mov     si, [bp + 0] ; caller IP
-	lodsb
-	cmp     al, 0C8h
-	ja		above_enter
-	je		emulate_enter
+    mov     bp, sp
+    mov     ds, [bp + 2] ; caller CS
+    mov     si, [bp + 0] ; caller IP
+    lodsb
+    cmp     al, 0C8h
+    ja      above_enter
+    je      emulate_enter
 below_enter:
-	cmp		al, 068h
-	je		emulate_push16
+    cmp     al, 068h
+    je      emulate_push16
     jnb     NOT_below_push16
-	jmp		below_push16
+    jmp     below_push16
 NOT_below_push16:
-	cmp     al, 06Ah
-	ja		between_6A_and_C8
-	jnb		NOT_emulate_imul
+    cmp     al, 06Ah
+    ja      between_6A_and_C8
+    jnb     NOT_emulate_imul
     jmp     emulate_imul
 NOT_emulate_imul:
-	lodsb
-	cbw
-	jmp		pushcommon
+    lodsb
+    cbw
+    jmp     pushcommon
 above_enter:
-	cmp		al, 0C9h
-	je		emulate_leave
-	jmp		SHORT emu_exit
+    cmp     al, 0C9h
+    je      emulate_leave
+    jmp     SHORT emu_exit
 between_6A_and_C8:
-	cmp		al, 0C0h
-	jb		between_6A_and_C0
-	cmp		al, 0C1h
-	ja		between_C1_and_C8
+    cmp     al, 0C0h
+    jb      between_6A_and_C0
+    cmp     al, 0C1h
+    ja      between_C1_and_C8
 
-	add		al, 12h		; C0 -> D2, C1 -> D3
-	mov		BYTE PTR [cs:sr_patch_opcode], al
+    add     al, 12h     ; C0 -> D2, C1 -> D3
+    mov     BYTE PTR [cs:sr_patch_opcode], al
     add     al, 86h - 0D2h ; generate 8-bit/16-bit XCHG opcode
-	mov		BYTE PTR [cs:sr_xchg1], al
-	mov		BYTE PTR [cs:sr_xchg2], al
-	lodsb				; Fetch mod/RM byte
-	mov		ah, al
-	or		al, 0C0h	; set memory operand to AX/AL (for shift/rotate)
-	and		al, 0F8h
-	mov		BYTE PTR [cs:sr_patch_opcode+1], al
-	and		ah, 0C7h	; set register operand to AX (for XCHG instructions)
-	mov		BYTE PTR [cs:sr_xchg2 + 1], ah
-	mov		BYTE PTR [cs:sr_xchg1 + 1], ah
-	cmp		ah, 06h		; special encoding for baseless 16-bit address
-	jne		not_mem16
-	mov		ah, 80h		; replace mode by something with a 16-bit offset
+    mov     BYTE PTR [cs:sr_xchg1], al
+    mov     BYTE PTR [cs:sr_xchg2], al
+    lodsb               ; Fetch mod/RM byte
+    mov     ah, al
+    or      al, 0C0h    ; set memory operand to AX/AL (for shift/rotate)
+    and     al, 0F8h
+    mov     BYTE PTR [cs:sr_patch_opcode+1], al
+    and     ah, 0C7h    ; set register operand to AX (for XCHG instructions)
+    mov     BYTE PTR [cs:sr_xchg2 + 1], ah
+    mov     BYTE PTR [cs:sr_xchg1 + 1], ah
+    cmp     ah, 06h     ; special encoding for baseless 16-bit address
+    jne     not_mem16
+    mov     ah, 80h     ; replace mode by something with a 16-bit offset
 not_mem16:
-	sub		ah, 40h		; 00..3F = 8bit, 40..7F = 16bit, 80..FF = nothing
-	js		no_offset
-	cmp		ah, 40h
-	jae		offset16
-	lodsb
-	mov     ah, 90h
-	jmp		SHORT ofs_common
+    sub     ah, 40h     ; 00..3F = 8bit, 40..7F = 16bit, 80..FF = nothing
+    js      no_offset
+    cmp     ah, 40h
+    jae     offset16
+    lodsb
+    mov     ah, 90h
+    jmp     SHORT ofs_common
 offset16:
-	lodsw
-	jmp		SHORT ofs_common
+    lodsw
+    jmp     SHORT ofs_common
 no_offset:
-	mov		ax, 9090h
+    mov     ax, 9090h
 ofs_common:
-	mov		WORD PTR [cs:sr_xchg2 + 2], ax
-	mov		WORD PTR [cs:sr_xchg1 + 2], ax
-	lodsb
-	mov		BYTE PTR [cs:sr_patch_count], al
-	mov     [bp + 0], OFFSET sr_inject ; return IP
+    mov     WORD PTR [cs:sr_xchg2 + 2], ax
+    mov     WORD PTR [cs:sr_xchg1 + 2], ax
+    lodsb
+    mov     BYTE PTR [cs:sr_patch_count], al
+    mov     [bp + 0], OFFSET sr_inject ; return IP
 exit_for_reenter:
-	mov		[bp + 2], cs ; return CS
+    mov     [bp + 2], cs ; return CS
     and     BYTE PTR [bp + 5], NOT 1        ; Clear TF
-	mov		WORD PTR [cs:reenter_offset], si
-	mov     WORD PTR [cs:reenter_segment], ds
+    mov     WORD PTR [cs:reenter_offset], si
+    mov     WORD PTR [cs:reenter_segment], ds
 below_push16:
 between_C1_and_C8:
 emu_exit:
-	mov		si, 1234h
-	emuss_oldds = $-2
-	mov		ds, si
-	mov     si, 1234h
-	emuss_oldsi = $-2
-	mov     bp, 1234h
-	emuss_oldbp = $-2
-	mov     ax, 1234h
-	emuss_oldax = $-2
-	iret
+    mov     si, 1234h
+    emuss_oldds = $-2
+    mov     ds, si
+    mov     si, 1234h
+    emuss_oldsi = $-2
+    mov     bp, 1234h
+    emuss_oldbp = $-2
+    mov     ax, 1234h
+    emuss_oldax = $-2
+    iret
 between_6A_and_C0:
     cmp     al, 6Bh
-	jnz     emu_exit
+    jnz     emu_exit
 emulate_imul:
     push    bx
     mov     ah, al
@@ -330,19 +330,19 @@ imul_immed_done:
 ;     the self-modifying, non-reentrant code here. This does not matter
 ;     until we add a TF setting interrupt handler wrapper, though.
 sr_inject:
-	sr_xchg1 = $
-	xchg	ax, WORD PTR [ds:1234h]		; TODO: segment prefixes
-	push	cx
-	mov		cl, 12h
-	sr_patch_count = $-1
-	sr_patch_opcode = $
-	shl		ax, cl
-	pop		cx
-	sr_xchg2 = $
-	xchg	ax, WORD PTR [ds:1234h]		; TODO: segment prefixes
+    sr_xchg1 = $
+    xchg    ax, WORD PTR [ds:1234h]     ; TODO: segment prefixes
+    push    cx
+    mov     cl, 12h
+    sr_patch_count = $-1
+    sr_patch_opcode = $
+    shl     ax, cl
+    pop     cx
+    sr_xchg2 = $
+    xchg    ax, WORD PTR [ds:1234h]     ; TODO: segment prefixes
 prepare_reenter:
     pushf
-	mov		WORD PTR [cs:emuss_oldbp], bp
+    mov     WORD PTR [cs:emuss_oldbp], bp
     mov     bp, sp
     or      BYTE PTR [bp + 1], 1        ; Re-Set TF
     mov     bp, 1234h
--- xtize.c
+++ xtize.c
@@ -6,38 +6,38 @@
 typedef unsigned char byte;
 
 struct ExecParameters {
-	unsigned env_segment;
-	char far* cmdline;
-	struct fcb far* arg1;
-	struct fcb far* arg2;
-	byte far* entry_stack;
-	void (far* __cdecl entry_point)(int dummy);
-	unsigned psp_segment;
+    unsigned env_segment;
+    char far* cmdline;
+    struct fcb far* arg1;
+    struct fcb far* arg2;
+    byte far* entry_stack;
+    void (far* __cdecl entry_point)(int dummy);
+    unsigned psp_segment;
 };
 
 void setpsp(unsigned newpsp)
 {
-	union REGS r;
-	r.h.ah = 0x50;
-	r.x.bx = newpsp;
-	intdos(&r, &r);
+    union REGS r;
+    r.h.ah = 0x50;
+    r.x.bx = newpsp;
+    intdos(&r, &r);
 }
 
 int LoadForDebugging(const char *program, struct ExecParameters *state)
 {
-	union REGS r;
-	struct SREGS sr;
-	r.x.ax = 0x4B01;
-	sr.es  = FP_SEG((void far*)state);
-	r.x.bx = FP_OFF((void far*)state);
-	sr.ds  = FP_SEG((void far*)program);
-	r.x.dx = FP_OFF((void far*)program);
-	intdosx(&r, &r, &sr);
-	if (r.x.cflag)
-		return -1;
-	state->psp_segment = getpsp();
-	setpsp(_psp);
-	return 0;
+    union REGS r;
+    struct SREGS sr;
+    r.x.ax = 0x4B01;
+    sr.es  = FP_SEG((void far*)state);
+    r.x.bx = FP_OFF((void far*)state);
+    sr.ds  = FP_SEG((void far*)program);
+    r.x.dx = FP_OFF((void far*)program);
+    intdosx(&r, &r, &sr);
+    if (r.x.cflag)
+        return -1;
+    state->psp_segment = getpsp();
+    setpsp(_psp);
+    return 0;
 }
 
 void far EnterSingleStep(struct ExecParameters far *state);
@@ -47,134 +47,134 @@ void interrupt EmulatingSS();
 
 void do_assert_eq(const char* x, unsigned v1, unsigned v2)
 {
-	if (v1 != v2)
-		printf("FAIL: %s - %04X != %04X\n", x, v1, v2);
+    if (v1 != v2)
+        printf("FAIL: %s - %04X != %04X\n", x, v1, v2);
 }
 
 #define ASSERT_EQ(word1, word2) do_assert_eq(#word1 ## " == " ## #word2, word1, word2)
 
 int main(int argc, char** argv)
 {
-	static int i = 0x1234;
-	struct ExecParameters p;
-	char *target;
+    static int i = 0x1234;
+    struct ExecParameters p;
+    char *target;
     p.env_segment = 0;
-	p.cmdline = MK_FP(_psp, 0x81);
+    p.cmdline = MK_FP(_psp, 0x81);
     p.arg1 = 0;
     p.arg2 = 0;
 
-	if (argc == 1)
-	{
-		fputs("Missing program name\n", stderr);
-		return 2;
-	}
-	if (stricmp(argv[1], "/T") == 0)
-	{
-		unsigned old_sp = _SP;
-		unsigned new_sp;
-		unsigned new_bp;
-		unsigned leave_sp;
-		unsigned old_bp = _BP;
-		unsigned leave_bp;
-		unsigned shl_80_3;
-		unsigned char rol_F7_2;
-		unsigned mul_3_4, mul_6_8, mul_2_5, mul_7_11;
-		puts("Test mode.");
-		setvect(0xA1, EmulatingSS);
-		asm {
-			int 0xA1
-			//enter 10, 0
-			db 0xC8, 0x0A, 0x00, 0x00
-			mov		cx, sp
-			mov		dx, bp
-			int 0xA1
-			// push 0
-			db 0x6A, 0x00
-			int 0xA1
-			// push 0x80
-			db 0x68, 0x80, 0x00
-			int 0xA1
-			// shl [WORD PTR bp-14], 3
-			db 0xC1, 0x66, 0xF2, 0x03
-			mov     bx, WORD PTR [bp-14]
-			//leave
-			int 0xA1
-			db 0xC9
+    if (argc == 1)
+    {
+        fputs("Missing program name\n", stderr);
+        return 2;
+    }
+    if (stricmp(argv[1], "/T") == 0)
+    {
+        unsigned old_sp = _SP;
+        unsigned new_sp;
+        unsigned new_bp;
+        unsigned leave_sp;
+        unsigned old_bp = _BP;
+        unsigned leave_bp;
+        unsigned shl_80_3;
+        unsigned char rol_F7_2;
+        unsigned mul_3_4, mul_6_8, mul_2_5, mul_7_11;
+        puts("Test mode.");
+        setvect(0xA1, EmulatingSS);
+        asm {
+            int 0xA1
+            //enter 10, 0
+            db 0xC8, 0x0A, 0x00, 0x00
+            mov     cx, sp
+            mov     dx, bp
+            int 0xA1
+            // push 0
+            db 0x6A, 0x00
+            int 0xA1
+            // push 0x80
+            db 0x68, 0x80, 0x00
+            int 0xA1
+            // shl [WORD PTR bp-14], 3
+            db 0xC1, 0x66, 0xF2, 0x03
+            mov     bx, WORD PTR [bp-14]
+            //leave
+            int 0xA1
+            db 0xC9
 
-			mov     [new_sp], cx
-			mov     [new_bp], dx
-			mov     [shl_80_3], bx
+            mov     [new_sp], cx
+            mov     [new_bp], dx
+            mov     [shl_80_3], bx
 
-			mov al, 0xF7
-			int 0xA1
-			//rol al, 2
-			db 0xC0, 0xC0, 2
-			mov		[rol_F7_2], al
-			int 0xA1
-			//ror [i], 3
-			db 0xC1, 0x0E
-			dw offset i
-			db 3
+            mov al, 0xF7
+            int 0xA1
+            //rol al, 2
+            db 0xC0, 0xC0, 2
+            mov     [rol_F7_2], al
+            int 0xA1
+            //ror [i], 3
+            db 0xC1, 0x0E
+            dw offset i
+            db 3
 
-			mov		ax, 3
-			int 0xA1
-			//imul	bx, ax, 4
-			db		0x6B, 0xD8, 0x04
-			mov		[mul_3_4], bx
+            mov     ax, 3
+            int 0xA1
+            //imul  bx, ax, 4
+            db      0x6B, 0xD8, 0x04
+            mov     [mul_3_4], bx
 
-			mov		bx, 6
-			int 0xA1
-			// imul	ax, bx, 8
-			db		0x6B, 0xC3, 0x08
-			mov		[mul_6_8], ax
+            mov     bx, 6
+            int 0xA1
+            // imul ax, bx, 8
+            db      0x6B, 0xC3, 0x08
+            mov     [mul_6_8], ax
 
-			mov		cx, 2
-			int 0xA1
-			// imul    bx, cx, 5
-			db		0x6B, 0xD9, 0x05
-			mov		[mul_2_5], bx
+            mov     cx, 2
+            int 0xA1
+            // imul    bx, cx, 5
+            db      0x6B, 0xD9, 0x05
+            mov     [mul_2_5], bx
 
-			mov		ax, 7
-			int 0xA1
-			//imul    ax, ax, 11
-			db		0x6B, 0xC0, 0x0B
-			mov		[mul_7_11], ax
-		}
-		leave_bp = _BP;
-		leave_sp = _SP;
-		ASSERT_EQ(old_sp,      leave_sp);
-		ASSERT_EQ(old_sp - 2,  new_bp);
-		ASSERT_EQ(old_sp - 12, new_sp);
-		ASSERT_EQ(old_bp,      leave_bp);
-		ASSERT_EQ(0x80 << 3,   shl_80_3);
-		ASSERT_EQ(0xDF,        rol_F7_2);
-		ASSERT_EQ((0x1234 >> 3) | 0x8000, i);
-		ASSERT_EQ(3*4,         mul_3_4);
-		ASSERT_EQ(6*8,         mul_6_8);
-		ASSERT_EQ(2*5,         mul_2_5);
-		ASSERT_EQ(7*11,        mul_7_11);
-		return 0;
-	}
-	target = searchpath(argv[1]);
-	if (target == NULL)
-	{
-		fprintf(stderr, "%s not found\n", argv[1]);
-		return 1;
-	}
+            mov     ax, 7
+            int 0xA1
+            //imul    ax, ax, 11
+            db      0x6B, 0xC0, 0x0B
+            mov     [mul_7_11], ax
+        }
+        leave_bp = _BP;
+        leave_sp = _SP;
+        ASSERT_EQ(old_sp,      leave_sp);
+        ASSERT_EQ(old_sp - 2,  new_bp);
+        ASSERT_EQ(old_sp - 12, new_sp);
+        ASSERT_EQ(old_bp,      leave_bp);
+        ASSERT_EQ(0x80 << 3,   shl_80_3);
+        ASSERT_EQ(0xDF,        rol_F7_2);
+        ASSERT_EQ((0x1234 >> 3) | 0x8000, i);
+        ASSERT_EQ(3*4,         mul_3_4);
+        ASSERT_EQ(6*8,         mul_6_8);
+        ASSERT_EQ(2*5,         mul_2_5);
+        ASSERT_EQ(7*11,        mul_7_11);
+        return 0;
+    }
+    target = searchpath(argv[1]);
+    if (target == NULL)
+    {
+        fprintf(stderr, "%s not found\n", argv[1]);
+        return 1;
+    }
 
-	while(*p.cmdline == ' ')
-		p.cmdline++;
-	while(*p.cmdline > ' ')
-		p.cmdline++;
+    while(*p.cmdline == ' ')
+        p.cmdline++;
+    while(*p.cmdline > ' ')
+        p.cmdline++;
     p.cmdline--;
     *p.cmdline = *(unsigned char*)(MK_FP(_psp, 0x80)) - (FP_OFF(p.cmdline) - 0x80);
-	if (LoadForDebugging(target, &p) < 0)
-	{
-		fputs("Error\n", stderr);
-		return 1;
-	}
-	setvect(1, EmulatingSS);
-	EnterSingleStep(&p);
-	//printf("\n%ld steps observed", GetCount());
-	return 0;
+    if (LoadForDebugging(target, &p) < 0)
+    {
+        fputs("Error\n", stderr);
+        return 1;
+    }
+    setvect(1, EmulatingSS);
+    EnterSingleStep(&p);
+    //printf("\n%ld steps observed", GetCount());
+    return 0;
 }
